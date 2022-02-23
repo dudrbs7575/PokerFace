39f81d40d21b99085ba38ab649f9f4b3386e094f
syg <dudrbs7575@naver.com>
2022-02-23 13:49:13 +0900

    signaling with websocket

--- signaling/signaling.js
+++ signaling/signaling.js
@@ -24,6 +24,25 @@ const pc_config = {
 let pc = new RTCPeerConnection(pc_config);
 let webSocket = new WebSocket(webSocketUri);
 
+//웹 소켓 연결 수립 후 send메세지로 확인
+
+//연결을 맺는것은 비동기 작업
+//WebSocket 오브젝트를 생성하자마자 send()로 데이터 전송을 시도하는 것은 성공하지 않을 확률 존재
+//연결이 수립된 이후에만 데이터를 전송하도록 하기 위해 onopen 핸들러 정의하고 이 위에서 작업.
 webSocket.onopen() = function() {
+    console.log("connected to the signaling server");
+
+    webSocket.send("msg to the signaling server");
 
 }
+
+//signaling server로 메세지를 보내기 위함.
+function send(msg) {
+    webSocket.send(JSON.stringify(msg));
+}
+
+webSocket.onmessage = function(msg) {
+    console.log(msg);
+}
+
+//exampleSocket.close(); web소켓 사용 마쳤을때 호출.
